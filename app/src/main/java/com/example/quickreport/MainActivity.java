ca9527a4302ef6e302bbc50f6762aa1db80456be
Aditya Galande <adityagalande14@gmail.com>
2021-01-02 16:08:03 +0530

    http updated

--- app/src/main/java/com/example/quickreport/MainActivity.java
+++ app/src/main/java/com/example/quickreport/MainActivity.java
@@ -15,8 +15,9 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String SAMPLE_JSON_RESPONSE ="https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&orderby=time&minmag=2.5&limit=150";
+    private static final String SAMPLE_JSON_RESPONSE = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&orderby=time&minmag=2.5&limit=150";
     private EarthquakeAdapter mAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,22 +43,22 @@ public class MainActivity extends AppCompatActivity {
         task.execute(SAMPLE_JSON_RESPONSE);
     }
 
-    private class EarthquakeAsyncTask extends AsyncTask<String, Void, ArrayList<EarthquakeData>>{
+    private class EarthquakeAsyncTask extends AsyncTask<String, Void, ArrayList<EarthquakeData>> {
 
         @Override
         protected ArrayList<EarthquakeData> doInBackground(String... urls) {
 
-            if(urls.length < 1 ){
+            if (urls.length < 1) {
                 return null;
             }
             ArrayList<EarthquakeData> result = QueryUtils.fetchEarthquakeData(urls);
             return result;
         }
 
-        protected void onPostExecute(ArrayList<EarthquakeData> data){
+        protected void onPostExecute(ArrayList<EarthquakeData> data) {
             mAdapter.clear();
 
-            if(data != null && !data.isEmpty()){
+            if (data != null && !data.isEmpty()) {
                 mAdapter.addAll(data);
             }
         }
